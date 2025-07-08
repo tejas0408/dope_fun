c4be30ef2078655c2ff1f618b4588bc16c295f14
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-07-08 08:28:28 +0530

    01: Setup

---
+++ dope/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Page =()=>{
+  return(
+    <div>
+      <Button variant={"destructive"}>
+
+      </Button>
+    </div>
+  )
+}
+
+export default Page;
